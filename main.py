dd9c7565b57a25f2d3ad35ce559011061013fb71
Trusty <sirjacobs.arthur@gmail.com>
2023-07-14 23:06:11 +0200

    Second commit

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 from flask_components.champion_bp import champion_bp
 
+from storage import player_storage, champ_storage
+
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(champion_bp, url_prefix='/champion')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    player_storage.test()
+    # app.run(debug=True)
--- storage.py
+++ storage.py
@@ -1,8 +1,15 @@
+import cassiopeia
 from tinydb import TinyDB, Query
 import os
+from cassiopeia import Summoner
+from dotenv import load_dotenv
+
+env_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(env_path)
+cassiopeia.set_riot_api_key(os.environ.get("RIOT_API_KEY"))
 
 
-class Champion:
+class Champions:
     def __init__(self):
         self.base_path = os.path.dirname(__file__)
         self.db_path = os.path.join(self.base_path, f'database/champions_db.json')
@@ -14,4 +21,15 @@ class Champion:
         return self.db.all()
 
 
-champ_storage = Champion()
+class Players:
+    def __init__(self):
+        self.usernames = ['TURBO Trusty','Ckwaceupoulet','TURBO OLINGO']
+
+    def test(self):
+        player = Summoner(name=self.usernames[0], region='EUW')
+        print(player.leagues)
+        # print([cm.champion.name for cm in good_with])
+
+
+champ_storage = Champions()
+player_storage = Players()
