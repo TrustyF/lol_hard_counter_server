0294454537fe953a9c4696c99d540d905e2a8722
Trusty <sirjacobs.arthur@gmail.com>
2023-08-20 20:16:42 +0200

    New stats, added lockout for rank hist

--- flask_components/player_bp.py
+++ flask_components/player_bp.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request
 from stores.manager import summ_manager
+import time
 
 player_bp = Blueprint('player', __name__)
+add_history_busy = False
 
 
 @player_bp.route("/get_all", methods=["GET"])
@@ -11,7 +13,21 @@ def get_all():
 
 @player_bp.route("/add_rank_to_history", methods=["GET"])
 def add_rank_to_history():
-    summ_manager.add_rank_to_history()
+    global add_history_busy
+    if not add_history_busy:
+        add_history_busy = True
+        summ_manager.add_rank_to_history()
+        # time.sleep(60)
+        add_history_busy = False
+        return {}, 200
+    else:
+        return {}, 404
+
+
+@player_bp.route("/update", methods=["GET"])
+def update():
+    time.sleep(10)
+    print('update')
     return {}, 200
 
 
